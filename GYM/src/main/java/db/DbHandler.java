0ea8e6e4d32812af7bb05b53f14c9fe6be83e39a
FranciscoBBruno <fransbebobruno@gmail.com>
2023-08-09 15:45:44 -0300

    cambios db

--- GYM/src/main/java/db/DbHandler.java
+++ GYM/src/main/java/db/DbHandler.java
@@ -1,5 +1,43 @@
 package db;
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class DbHandler {
 
+	private Connection conn;
+	private String host = "localhost";
+	private String db = "gimnasio";
+	private String user = "consultando";
+	private String password = "gimnasio345";
+	
+	public DbHandler() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private Connection getConnection() {
+		try {
+			if(conn==null || conn.isClosed())
+			conn=DriverManager.getConnection("jdbc:mysql://"+host+"/"+db, user, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+	
+	private void cerrarConnection() {
+		try {
+			if(conn!=null && !conn.isClosed()) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
