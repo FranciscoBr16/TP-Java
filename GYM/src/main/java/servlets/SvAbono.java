e9e9bea5f7fcb5729826c504dfd7d403ef857687
FranciscoBBruno <fransbebobruno@gmail.com>
2023-09-28 16:09:40 -0300

    modificacion en el sv de abono

--- GYM/src/main/java/servlets/SvAbono.java
+++ GYM/src/main/java/servlets/SvAbono.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import db.DbHandler;
 import entities.Abono;
 
-@WebServlet("/SvAbono")
+@WebServlet("/SvAbono /abonos")
 public class SvAbono extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
