9b7720617f16297dad1c8088d924e33c52bdea41
santiauat <santiauat@hotmail.com>
2023-10-05 17:07:30 -0300

    Se agrega nombre a la clase

--- BD/gimnasio.sql
+++ BD/gimnasio.sql
@@ -2,9 +2,9 @@ CREATE DATABASE  IF NOT EXISTS `gimnasio` /*!40100 DEFAULT CHARACTER SET utf8mb4
 USE `gimnasio`;
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
--- Host: localhost    Database: gimnasio
+-- Host: 127.0.0.1    Database: gimnasio
 -- ------------------------------------------------------
--- Server version	8.0.34
+-- Server version	8.0.25
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -53,6 +53,7 @@ DROP TABLE IF EXISTS `clase`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `clase` (
   `id_clase` int NOT NULL,
+  `nombre_clase` varchar(45) DEFAULT NULL,
   `descripcion` varchar(255) DEFAULT NULL,
   `cupo` int DEFAULT NULL,
   `horario` time DEFAULT NULL,
@@ -407,4 +408,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-10-05 16:53:59
+-- Dump completed on 2023-10-05 17:05:13
